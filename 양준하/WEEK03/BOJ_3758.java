1790367155b2e8073f11f2da94d86bd74cb09350
양준하 <dign552@naver.com>
2025-07-22 22:11:38 +0900

    양준하/BOJ/3758/248ms

--- 양준하/WEEK03/BOJ_3758.java
+++ 양준하/WEEK03/BOJ_3758.java
@@ -1,11 +1,7 @@
 package 양준하.WEEK03;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_3758 {
     static int T;
