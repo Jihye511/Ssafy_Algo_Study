8eaf94db4227f3d489f5ffc1b48f6851f8045c21
양준하 <dign552@naver.com>
2025-07-22 22:12:16 +0900

    양준하/BOJ/20310/108ms

---
+++ 양준하/WEEK03/BOJ_20310.java
@@ -0,0 +1,41 @@
+package 양준하.WEEK03;
+
+import java.io.*;
+
+public class BOJ_20310 {
+    static String S;
+    static int cnt0 = 0;
+    static int cnt1 = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = br.readLine().trim();
+
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '0') {
+                cnt0 += 1;
+            } else {
+                cnt1 += 1;
+            }
+        }
+
+        cnt0 /= 2;
+        cnt1 /= 2;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '0') {
+                if (cnt0 > 0) {
+                    cnt0 -= 1;
+                    sb.append('0');
+                } 
+            } else {
+                if (cnt1 <= 0) {
+                    sb.append('1');
+                }
+                cnt1 -= 1;
+            }
+        }
+        System.out.println(sb);
+    }
+}
