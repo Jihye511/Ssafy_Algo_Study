887426f2d8d82762f8f8b29780e420a1c45a487c
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-08-01 16:12:33 +0900

    오현석/BOJ/2075/900ms

---
+++ 오현석/WEEK04/BOJ_2075.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> topNum = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                topNum.add(tmp);
+                if (topNum.size()>N)topNum.poll();
+            }
+        }
+        System.out.println(topNum.poll());
+    }
+}
