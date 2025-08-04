34f4b4a8076b94822285af049b2e2218b746bb62
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2025-08-05 00:47:58 +0900

    우지연/BOJ/1138/144ms

---
+++ 우지연/WEEK05/Boj_1138.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1138 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        int[] intArr = new int[N+1];
+        int[] answer = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+for(int i=1;i<=N;i++){
+    intArr[i]=Integer.parseInt(st.nextToken());
+}
+        for(int i=1;i<=N;i++){
+            int j=1;
+
+            while(true){
+                if(intArr[i]==0&&answer[j]==0){
+                    answer[j]=i;
+                    break;
+                }
+                else if(answer[j]==0) {
+                    intArr[i]--;
+                }
+                    j++;
+                
+            }
+
+
+        }
+        for(int i = 1; i<=N; i++)
+            System.out.print(answer[i] +" ");
+    }
+}
