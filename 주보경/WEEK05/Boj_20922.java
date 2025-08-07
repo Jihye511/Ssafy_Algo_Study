8ae2dd3df84d14f9ccf5c093ababb322eb0e7e6b
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-08-07 21:47:16 +0900

    주보경/BOJ/20922/292ms

--- 주보경/WEEK05/Boj_20922.java
+++ 주보경/WEEK05/Boj_20922.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
