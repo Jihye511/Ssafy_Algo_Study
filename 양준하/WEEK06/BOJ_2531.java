26c7e7ce61737a3ae1268c51cff60a712937bd89
양준하 <dign552@naver.com>
2025-08-12 15:19:12 +0900

    양준하/BOJ/2531/128ms

---
+++ 양준하/WEEK06/BOJ_2531.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2531 {
+    static int n, d, k, c;
+    static int[] sushi;
+    static int[] count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 접시 수
+        d = Integer.parseInt(st.nextToken()); // 초밥 가짓수
+        k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수
+        c = Integer.parseInt(st.nextToken()); // 쿠폰 초밥 번호
+
+        sushi = new int[n];
+        for (int i = 0; i < n; i++) {
+            sushi[i] = Integer.parseInt(br.readLine());
+        }
+        
+        count = new int[d + 1];
+        int type = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (count[sushi[i]] == 0) {
+                type += 1;
+            }
+            count[sushi[i]] += 1;
+        }
+        
+        int max = type;
+        if (count[c] == 0) {
+            max = type + 1;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            int rmIdx = i;
+            int addIdx = (i + k) % n;
+            
+            count[sushi[rmIdx]] -= 1;
+            if (count[sushi[rmIdx]] == 0) {
+                type -= 1;
+            }
+            if (count[sushi[addIdx]] == 0) {
+                type += 1;
+            }
+
+            count[sushi[addIdx]] += 1;
+            
+            int cur = type;
+            if (count[c] == 0) {
+                cur += 1;
+            }
+            max = Math.max(max, cur);
+        }
+        
+        System.out.println(max);
+    }
+}
