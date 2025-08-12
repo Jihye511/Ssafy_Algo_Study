99d1f9295f8a7e7bf1b2eee44037b84ddec25235
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-12 10:53:12 +0900

    윤규성/BOJ/2531/240ms

---
+++ 윤규성/WEEK06/BOJ_2531.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2531 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int max = 0;
+
+    int N = Integer.parseInt(st.nextToken());
+    int d = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[N];
+    for(int i = 0; i< N; i++){
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    // 초기값 세팅
+    for(int i = 0; i < k; i++){
+      map.put(arr[i], map.getOrDefault(arr[i] ,  0) + 1);
+      max = Math.max(map.size(), max);
+    }
+
+    // 돌아
+    for(int i = 0; i < N; i++){
+      if(map.get(arr[i]) > 1) map.put(arr[i], map.get(arr[i]) - 1);
+      else map.remove(arr[i]);
+
+      int j = i + k;
+      map.put(arr[j % N], map.getOrDefault(arr[j % N], 0) + 1);
+      map.put(c, map.getOrDefault(c, 0) + 1);
+      max = Math.max(max, map.size());
+    }
+
+    System.out.println(max);
+
+  }
+}
