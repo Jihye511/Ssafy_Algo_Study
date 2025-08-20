d420edeeaba8ef553aaeb0e4302af90aa22ae098
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-08-20 10:09:11 +0900

    주보경

---
+++ 주보경/WEEK07/Boj_12919.py
@@ -0,0 +1,23 @@
+import sys
+ 
+sys.setrecursionlimit(3000)
+
+start =  input()
+target = input()
+
+def dfs(t):
+    if start == t:
+        print(1)
+        exit(0)
+
+    if len(t) == 0:
+        return
+        
+    if (t[-1] == 'A'):
+        dfs(t[:-1])
+    if t[0] == 'B':
+        new = t[1:]
+        dfs(new[::-1])
+
+dfs(target)
+print(0)
