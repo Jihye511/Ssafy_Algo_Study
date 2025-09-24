d7432c7852e8a408ab997da0a51585cbb7e43b09
huntedu <best00517@naver.com>
2025-09-24 22:44:27 +0900

    유환태/BOJ/2631/68ms

---
+++ 유환태/WEEK12/BOJ_2631.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int N;
+	static int[] arr;
+    
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+
+        int max = 1;
+
+        for(int i = 0; i < N; i++){
+            dp[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N-max);
+    }
+}
