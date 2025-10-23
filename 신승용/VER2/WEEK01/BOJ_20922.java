f81e026ce2c75afb112b8a755692577dc32c0cad
sso9594 <sso9594@naver.com>
2025-10-24 00:46:33 +0900

    신승용/BOJ/20922/436ms

---
+++ 신승용/VER2/WEEK01/BOJ_20922.java
@@ -0,0 +1,41 @@
+package 신승용.VER2.WEEK01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_20922 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
+        int left = 0;
+
+        for (int i = 0; i < N; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+            if(map.get(arr[i]) > K){
+                while(map.get(arr[i]) > K){
+                    map.put(arr[left], map.get(arr[left])-1);
+                    left++;
+                }
+            }
+
+            int len = i - left + 1;
+            answer = Math.max(answer, len);
+        }
+
+        System.out.println(answer);
+    }
+}
