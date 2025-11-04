5f55d149dcec238a6d6266639a3e279669ec63c1
sso9594 <sso9594@naver.com>
2025-11-05 00:56:18 +0900

    신승용/PGS/389479

---
+++ 신승용/VER2/WEEK03/PGS_389479.java
@@ -0,0 +1,31 @@
+package 신승용.VER2.WEEK03;
+
+import java.util.*;
+
+class PGS_389479 {
+    public int solution(int[] players, int m, int k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        int avail = 0;
+        int answer = 0;
+
+        for (int t = 0; t < players.length; t++) {
+            
+            if (queue.size() == k) {
+                avail -= queue.poll();
+            }
+
+            int add = players[t] / m;
+            
+            if (avail < add) {
+                int diff = add - avail;
+                queue.offer(diff);
+                
+                avail += diff;
+                answer += diff;
+            } else {
+                queue.offer(0);
+            }
+        }
+        return answer;
+    }
+}
